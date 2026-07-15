dac713c9be664f3205d8e741a6856cb30c7e3ba3
fparuce <felipe_costacurta@hotmail.com>
2026-07-15 17:46:29 -0300

    init

---
+++ src/main/java/com/itau/desafio/requester_service/RequesterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.itau.desafio.requester_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequesterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequesterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/desafio/requester_service/RequesterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.desafio.requester_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequesterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
