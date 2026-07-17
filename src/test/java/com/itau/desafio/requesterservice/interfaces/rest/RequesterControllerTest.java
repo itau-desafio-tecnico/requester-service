74098ed17a3fe3b8a84cdbd721dc9446037d0110
fparuce <felipe_costacurta@hotmail.com>
2026-07-16 22:56:47 -0300

    ci file and adjusts

--- src/test/java/com/itau/desafio/requesterservice/interfaces/rest/RequesterControllerTest.java
+++ src/test/java/com/itau/desafio/requesterservice/interfaces/rest/RequesterControllerTest.java
@@ -17,7 +17,6 @@ import tools.jackson.databind.ObjectMapper;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
